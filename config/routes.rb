759abd0fc6a5e5e98d37e983ecc32a9b085d77dc
khalil Frough <khalil.froughkmu@gmail.com>
2020-03-28 13:50:36 +1100

    created the links and made the simple app work

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_page#home'
   get 'static_page/home'
   get 'static_page/help'
   get 'static_page/about'
   get 'static_page/contact'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'static_page#home'
+  
 end
